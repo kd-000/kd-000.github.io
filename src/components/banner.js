35369d3f4946534692961bfbcd399507c4558f8c
Katherine Day <kgd272@student.bham.ac.uk>
2024-05-21 15:29:37 +0100

    Commented in Links

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,8 +3,8 @@ import '../css/banner.css';
 function Banner() {
   return (
     <div className="banner-outer">
-       {/* <a className="icon-bottom-item" href="https://www.linkedin.com/in/katherine-day-091b41206"><img className="icon" src="linkedin-black.png"></img></a> */}
-        {/* <a href="https://github.com/kd-000/minmax"><img className="icon" src="github-mark.svg" alt="GitHub Logo Link"></img></a> */}
+        <a className="icon-bottom-item" href="https://www.linkedin.com/in/katherine-day-091b41206"><img className="icon" src="linkedin-black.png"></img></a>
+        <a href="https://github.com/kd-000/minmax"><img className="icon" src="github-mark.svg" alt="GitHub Logo Link"></img></a>
     </div>
   );
 }
